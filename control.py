9a8d2ba929baa1a1f58421c89d1afaa31eb4a0c9
unknown <cozynye@gmail.com>
2021-01-15 15:42:44 +0900

    first

---
+++ control.py
@@ -0,0 +1,77 @@
+# 기본 if문 구조 ->바로 아래 문장부터 if문에 속하는 모든 문장에 들여쓰기를 해주어야 한다(다른 언어와의 차이점)
+# if 조건문:
+#     수행할 문장1
+#     수행할 문장2
+#     ...
+# else:
+#     수행할 문장A
+#     수행할 문장B
+#     ...
+
+a=int(input())
+if a==1 :
+    print('일입니다')
+# print('이건 안될걸')
+else :
+    print('1이 아니다')
+
+# elif : 다양한 조건을 판단하는 함수이며, 조건, 개수 제한 없이 사용할수 있다.
+# pass : if문에 아무런 결과 값도 실행하지 않게하는 역할을 한다.
+
+a = int(input())
+if a ==1:
+    print("일 입니다.")
+elif a==3:
+        print("삼 입니다.")
+elif a==4:
+        print("사 입니다.")
+else:
+        print("값이 없습니다.")
+
+if a==1:
+        pass
+else:
+        print("값이 없습니다.")
+
+
+# while : 해당 조건이 참인 경우 반복해서 수행
+# while < 조건문 >:
+#         < 수행할 문장1 >
+#         < 수행할 문장2 >
+#         < 수행할 문장3 >
+a=int(input())
+while a<10 :
+    a=a+1
+    print(a)
+    if(a==10) :
+        print('종료')
+
+
+b=10
+while b<30 :
+
+        b = b + 1
+        if b%2==0 : continue
+        print(b)
+        if(b==25) : break
+
+
+# for 변수 in 리스트(튜플, 문자열):
+#     < 실행할 문장1 >
+#     < 실행할 문장2 >
+
+list=['apple', 'banana', 'candy', 'dog']
+for i in list:
+    print(i)
+
+a = [90, 25, 67, 45, 80]
+n = 0
+for i in a:
+    n = n + 1
+    if i<60: continue
+    print("%d번 수험생 합격입니다." % n)#포매팅
+
+
+num=int(input())
+gugu=[num*b for b in range(1,10)]
+print(gugu)
---
+++ datatype.py
@@ -0,0 +1,42 @@
+# 파이썬의 자료형에는 크게 나누면 '수치형(numbers)', '논리형(bool)', '순서형(sequence)', '매핑형(mapping)',이 있다.
+
+# 수치형 - 정수(int), 더 큰 정수(long int), 소수(float), 복소수(complex)
+number_int=123
+print(type(number_int),number_int)
+
+number_float=1.4
+print(type(number_float),number_float)
+
+number_complex=3+3j
+print(type(number_complex),number_complex)
+
+# 논리형
+boolean_one=3<4
+print(type(boolean_one),boolean_one)
+
+boolean_two=11<10
+print(type(boolean_two),boolean_two)
+
+# 순서형 - 문자열(string), 리스트(list), 튜플(tuple) -> 사용자 정의 클래스
+string='test'
+print(type(string), string)
+
+# 리스트는 [ ] 사용하지만, 튜플은 () 사용한다.
+# 리스트는 변경이 가능하지만 문자열, 튜플은 요소 값 변경(수정, 삭제)이 안 된다.
+# 튜플은 요소 뒤에 ,을 사용해야 하며 괄호를 생략해도 된다.
+# 기타 사항
+
+list=[10,'list',30,'bye']
+print(type(list),list)
+
+tuple=(1,)
+print(type(tuple),tuple)
+tuple='test',2
+print(type(tuple),tuple)
+tuple=(1,2,3,4)
+print(type(tuple),tuple)
+
+
+# 매핑형 - 딕셔너리(dictionary), 클래스 인스턴스, C확장형 등
+dict={'one' : 1, 'two' : 'second', 'three' : 3}
+print(type(dict), dict)
